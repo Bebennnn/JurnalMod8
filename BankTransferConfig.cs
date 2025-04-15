d0f12b816c2306c42f05ad402eda87367f2076ee
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:13:21 +0700

    “aplikasi bank transfer dengan runtime configuration”

---
+++ BankTransferConfig.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Modul8_103022330122
+{
+    public class BankTransferConfig
+    {
+        private const string ConfigPath = "bank_transfer_config.json";
+
+        public string lang { get; set; } 
+       public Transfer trasnfer { get; set; }
+
+        public List<string> methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+
+
+        public BankTransferConfig()
+        {
+            LoadConfig();
+        }
+
+       
+        public class Transfer {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+        public class Confirmation { 
+            public string en { get; set; }
+            public string id { get; set; }
+
+        }
+
+        public void SetDefault()
+        {
+            BankTransferConfig config = new BankTransferConfig();
+            config.lang = "en";
+            config.trasnfer.threshold = 25000000;
+            config.trasnfer.low_fee = 6500;
+            config.trasnfer.high_fee = 15000;
+            config.methods = "[ “RTO (real-time)”, “SKN”, “RTGS”, “BI FAST” ]";
+            config.confirmation.en = "yes";
+            config.confirmation.id = "ya";
+
+
+        }
+
+        public void LoadConfig()
+        {
+            if (File.Exists(ConfigPath))
+            {
+                string json = File.ReadAllText(ConfigPath);
+                var data = JsonSerializer.Deserialize<BankTransferConfig>(json);
+                if (data != null)
+                {
+                    lang = data.lang;
+                    threshold = data.Transfer.threshold;
+                    low_fee = data.low_fee;
+                    high_fee = data.high_fee;
+                    methods = data.methods;
+                    en = data.en;
+                    id = data.id;
+                }
+            }
+            else
+            {
+                SaveConfig();
+            }
+        }
+        public void SaveConfig()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(this, options);
+            File.WriteAllText(ConfigPath, json);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Modul8_103022330122;
+
+class program
+{
+    static void Main(String[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        int Masukkan;
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        }
+        else
+        {
+            Console.WriteLine("“Masukkan jumlah uang yang akan di-transfer:");
+        }
+
+        Masukkan(Console.ReadLine());
+
+        if (config.tranfer >= 25000000)
+        {
+            Console.WriteLine(Masukkan - config.trasnfer.low_fee);
+        } else {
+            Console.WriteLine(Masukkan - config.transfer.high_fee);
+    }
+}
